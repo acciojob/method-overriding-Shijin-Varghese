d9725e56e76e52fdaebf73bc10b866d1668161de
Shijin-Varghese <108483342+Shijin-Varghese@users.noreply.github.com>
2023-04-11 15:32:40 +0530

    hi

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static void main(String args[]){
         B a=new B();
          a.meth();
-        a.meth();
+         System.out.println(a.meth());
+        System.out.println(a.meth());
     }
 }
