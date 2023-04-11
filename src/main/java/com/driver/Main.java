e09464336e96ed9ee884d45d7466128d6cf7b005
Shijin-Varghese <108483342+Shijin-Varghese@users.noreply.github.com>
2023-04-11 15:29:19 +0530

    added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
-
+import java.util.*;
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
 public class Main {
-  
+    public static void main(String args[]){
+        B a=new B();
+         a.meth();
+        a.meth();
+    }
 }
